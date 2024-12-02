00582029d9ad05ab59dd67971f98da90079e0c8f
moizeey <moizeey17@gmail.com>
2024-12-02 22:29:11 +0500

    updated nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { navLinks } from "../constants";
 import { UseMenu } from "../context/navContext";
 import { RxCross1 } from "react-icons/rx";
